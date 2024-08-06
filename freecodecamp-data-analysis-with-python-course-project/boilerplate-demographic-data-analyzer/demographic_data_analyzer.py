da7719a21bf06bb8952071a20b55b0b9465f049e
vutuankiet4599 <vutuankiet4599@gmail.com>
2024-08-06 21:20:31 +0700

    complete project 1+2

--- freecodecamp-data-analysis-with-python-course-project/boilerplate-demographic-data-analyzer/demographic_data_analyzer.py
+++ freecodecamp-data-analysis-with-python-course-project/boilerplate-demographic-data-analyzer/demographic_data_analyzer.py
@@ -3,42 +3,43 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df.groupby('race')['race'].count()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = round(df[df['sex'] == 'Male']['age'].mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = round((df[df['education'] == 'Bachelors'].shape[0] / float(df.shape[0])) * 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    higher_education = df[df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
+    lower_education = df[~df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])]
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    lower_education_rich = round((lower_education[df['salary'] == '>50K'].shape[0] / lower_education.shape[0]) * 100, 1)
+    higher_education_rich = round((higher_education[df['salary'] == '>50K'].shape[0] / higher_education.shape[0]) * 100, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    num_min_workers = df[df['hours-per-week'] == min_work_hours]['hours-per-week'].count()
 
-    rich_percentage = None
+    rich_percentage = round((df[(df['hours-per-week'] == 1) & (df['salary'] == '>50K')]['hours-per-week'].count() / num_min_workers) * 100, 1)
 
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    country_percent = df[df['salary'] == '>50K']['native-country'].value_counts() / df['native-country'].value_counts()
+    highest_earning_country =country_percent.idxmax()
+    highest_earning_country_percentage = round(country_percent[highest_earning_country] * 100, 1)
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    top_IN_occupation = df[(df['salary'] == '>50K') & (df['native-country']  == 'India')]['occupation'].value_counts().idxmax()
 
     # DO NOT MODIFY BELOW THIS LINE
 
--- freecodecamp-data-analysis-with-python-course-project/boilerplate-mean-variance-standard-deviation-calculator/mean_var_std.py
+++ freecodecamp-data-analysis-with-python-course-project/boilerplate-mean-variance-standard-deviation-calculator/mean_var_std.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 def calculate(list):
-
-
-
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+    calculations = {}
+    matrix = np.array(list).reshape(3,3)
+    calculations['mean'] = [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean()]
+    calculations['variance'] = [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var()]
+    calculations['standard deviation'] = [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std()]
+    calculations['max'] = [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max()]
+    calculations['min'] = [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min()]
+    calculations['sum'] = [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum()]
 
     return calculations
